4066b1da8644074751209a03ed61784717dc8409
Luong Quang Manh <luongquangmanh85@gmail.com>
2018-11-15 16:51:52 +0700

    Add Bootstrap. Make intro page

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,2 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import $ from 'jquery'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+
+import TopBar from '../components/topbar'
+import Section from '../components/sections'
+
+
+storiesOf('TopBar', module)
+  .add('without props', () => <TopBar />)
+
+storiesOf('Section', module)
+  .add('without props', () => <Section />)
+  .add('with children', () => <Section>Hello World</Section>)
+  .add('with background color', () => <Section backgroundColor="#d32f2f">Hello World</Section>)
